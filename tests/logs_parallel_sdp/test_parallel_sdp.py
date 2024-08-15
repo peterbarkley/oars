00114f65c5b18521de3a57f5ff65463111c823dd
Peter Barkley <peter.barkley@gmail.com>
2024-08-15 09:20:16 -0700

    Add parallel test files

--- tests/logs_parallel_sdp/test_parallel_sdp.py
+++ tests/logs_parallel_sdp/test_parallel_sdp.py
@@ -15,13 +15,14 @@ def testSDP(tgt_n=3, parallel=False, vartol=1e-4, verbose=False):
     data = [(2*tgt_n, 2*tgt_n), {'A':Ki, 'v':1}, {'A':K1, 'v':0}, -Ko] + Kp
     dim = len(data)
     Zd, Wd = getTwoBlockSLEM(dim)
-    x, results = solve(dim, data, proxlist, W=Wd, Z=Zd, parallel=parallel, itrs=500, gamma=0.8, checkperiod=10, vartol=vartol, verbose=verbose) #vartol=1e-5, 
+    x, results = solve(dim, data, proxlist, W=Wd, Z=Zd, parallel=parallel, itrs=100, gamma=0.8, checkperiod=10, vartol=vartol, verbose=verbose) #vartol=1e-5, 
     # print(x)
     # print(results)
     # Save results
     for i, log in enumerate(results):
-        with open('parallel_logs_'+str(i)+'.json', 'w') as f:
-            json.dump(log, f)
+        if log['log'] is not None:
+            with open('parallel_logs_'+str(i)+'.json', 'w') as f:
+                json.dump(log['log'], f)
     print(np.trace(Ko @ x))
     from oars.pep import getContractionFactor
     print(getContractionFactor(Z, W))
